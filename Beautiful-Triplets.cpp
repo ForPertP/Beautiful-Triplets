712e7ea2a79e6ad1dded9bd8d4910595cc5508f7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-26 15:25:15 +0900

    Create Beautiful-Triplets.cpp

---
+++ Beautiful-Triplets.cpp
@@ -0,0 +1,15 @@
+int beautifulTriplets(int d, vector<int> arr)
+{
+    auto check = [&](int n)
+    {
+        return std::binary_search(begin(arr), end(arr), n);
+    };
+
+    auto result = std::count_if( begin(arr), end(arr),
+    [&](int n)
+    {
+        return check(d+n) && check(2*d + n);
+    });
+    
+    return result;
+}
