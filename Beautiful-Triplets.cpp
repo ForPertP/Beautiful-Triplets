9bb1a1856f7d3a908770214274f50636d16e5ffe
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-04-17 14:44:33 +0900

    Update Beautiful-Triplets.cpp

--- Beautiful-Triplets.cpp
+++ Beautiful-Triplets.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'beautifulTriplets' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
 int beautifulTriplets(int d, vector<int> arr)
 {
     auto check = [&arr](int n)
